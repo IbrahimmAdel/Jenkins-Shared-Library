8e9d200e7d772ceb1b922f98750ffc7d99ea9d04
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-30 14:53:44 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,12 +1,16 @@
 #!usr/bin/env groovy
-def call(String imageName, String deploymentFileName, String openshiftCredentialsID, String openshiftServerURL, String openshifProject) {
+def call(String imageName, deploymentFileName, String openshifProject, String openshiftCluster)//String imageName, String deploymentFileName, String openshiftCredentialsID, String openshiftServerURL, String openshifProject) {
 	// Update deployment.yaml with new Docker Hub image
         sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
                     
         // Log in to OpenShift with serviceaccount
-	withCredentials([string(credentialsId: "${openshiftCredentialsID}", variable: 'OPENSHIFT_TOKEN')]) {
-                sh "oc login --token=${OPENSHIFT_TOKEN} --server=${openshiftServerURL} --insecure-skip-tls-verify"
-        	echo "Deploying the APP to OpenShift..."
-        	sh "oc apply -f . -n ${openshifProject}"
-        }      
+	sh "oc apply -f . -n ${openshiftProject} --server=${openshiftCluster}"
+
+
+	
+	//withCredentials([string(credentialsId: "${openshiftCredentialsID}", variable: 'OPENSHIFT_TOKEN')]) {
+          //      sh "oc login --token=${OPENSHIFT_TOKEN} --server=${openshiftServerURL} --insecure-skip-tls-verify"
+        //	echo "Deploying the APP to OpenShift..."
+        //	sh "oc apply -f . -n ${openshifProject}"
+        //}      
 }
