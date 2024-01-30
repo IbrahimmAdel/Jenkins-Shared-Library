d728b79b02656dfd8fd01a2ba77862e9d5d09b8e
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-30 14:12:47 +0200

    Update runSonarQubeAnalysis.groovy

--- vars/runSonarQubeAnalysis.groovy
+++ vars/runSonarQubeAnalysis.groovy
@@ -1,12 +1,12 @@
 #!usr/bin/env groovy
-def call(String sonarqubeServerName, String sonarqubeAuthenticationToken, String sonarqubeProjectKey, String sonarqubeHostServer) {
-	withSonarQubeEnv("${sonarqubeServerName}") {
-	        withCredentials([string(credentialsId: "${sonarqubeAuthenticationToken}", variable: 'token')]) {
+def call(){ //String sonarqubeServerName, String sonarqubeAuthenticationToken, String sonarqubeProjectKey, String sonarqubeHostServer) {
+	withSonarQubeEnv() { //"${sonarqubeServerName}"
+	        //withCredentials([string(credentialsId: "${sonarqubeAuthenticationToken}", variable: 'token')]) {
         		echo "Running SonarQube Analysis..."
-        		sh "./gradlew sonar \
-        		    -Dsonar.projectKey=${sonarqubeProjectKey} \
-        		    -Dsonar.host.url='http://${sonarqubeHostServer}:9000' \
-        		    -Dsonar.login=${token}"       			    
-		}
+        		sh "./gradlew sonar" //\
+        		    //-Dsonar.projectKey=${sonarqubeProjectKey} \
+        		    //-Dsonar.host.url='http://${sonarqubeHostServer}:9000' \
+        		    //-Dsonar.login=${token}"       			    
+		//}
 	}
 }
