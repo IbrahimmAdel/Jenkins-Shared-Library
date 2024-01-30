766380fc8c327ba61cdb8887cec4031fc061f16e
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-31 01:50:43 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,4 +1,4 @@
-//#!/usr/bin/env groovy
+#!/usr/bin/env groovy
 //def call(String openshiftCluster, String openshiftProject, String imageName) {
     // Update deployment.yaml with new Docker Hub image
    // sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
@@ -12,7 +12,7 @@
     //    }
   //  }
 //}
-#!/usr/bin/env groovy
+
 def call(String openshiftCluster, String openshiftProject, String imageName) {
     // Update deployment.yaml with new Docker Hub image
     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
