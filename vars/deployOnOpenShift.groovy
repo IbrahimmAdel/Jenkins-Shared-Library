1f8a3e07e9af43baf124293f5249beaef896c442
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-31 01:15:17 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -14,21 +14,28 @@
 //	}
 //}
 
-def call(String openshiftCluster, String openshiftProject, String imageName) {
+
+def call(String openshiftProject, String imageName) {
     // Update deployment.yaml with new Docker Hub image
     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
-    // Login and Deploy on OpenShift cluster
-    openshift.withCluster(
-        cluster: openshiftCluster,
-        serverUrlVariable: 'serverURL', // Use serverUrlVariable instead of serverurlVariable
-        tokenVariable: 'token'
-    ) {
-        openshift.withProject("${openshiftProject}") {
-            // Use the variable serverURL here instead of ${serverURL}
-            sh "oc login --server=${serverURL} --token=${token} --insecure-skip-tls-verify"
-            sh "oc project ${openshiftProject}"
-            sh "oc apply -f ."
+    // Retrieve OpenShift credentials from Jenkins credentials store
+    withCredentials([openshiftServiceAccount(credentialsId: 'OpenShift-token', variable: 'openshiftCredentials')]) {
+        // Login and Deploy on OpenShift cluster
+        openshift.withCluster(
+            cluster: 'OpenShift',
+            serverUrlVariable: 'serverURL', 
+            credentialsId: '${openshiftCredentials}'
+        ) {
+            openshift.withProject("${openshiftProject}") {
+                script {
+                    // Use the 'serverURL' variable here
+                    sh "oc login --token=${openshiftCredentials} --insecure-skip-tls-verify"
+                    sh "oc project ${openshiftProject}"
+                    sh "oc apply -f ."
+                }
+            }
         }
     }
 }
+
