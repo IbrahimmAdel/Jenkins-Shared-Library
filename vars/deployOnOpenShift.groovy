dd404d415a6a23a6ec8970a107c3d920113af3dc
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-30 15:43:05 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,4 +1,4 @@
-#!usr/bin/env groovy
+//#!usr/bin/env groovy
 //def call(String imageName, String deploymentFileName, String openshifProject, String openshiftCluster){ //String imageName, String deploymentFileName, String openshiftCredentialsID, String openshiftServerURL, String openshifProject) {
 	// Update deployment.yaml with new Docker Hub image
   //      sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
@@ -23,5 +23,16 @@
     // Log in to OpenShift with serviceaccount
    // sh "oc apply -f . -n ${openshiftProject} --server=${openshiftCluster}"
 //}
+#!usr/bin/env groovy
+def call(String imageName ,String deploymentFileName ,String openshiftProject ,String openshiftCluster) {
+	sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
+	sh "oc apply -f . -n ${openshiftProject} --server=${openshiftCluster}"
 
-def
+
+
+	
+}
+
+
+
+
