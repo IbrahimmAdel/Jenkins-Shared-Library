df0267bc589af140bb07a6069054ef9c3549de9a
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-31 01:33:52 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -14,14 +14,16 @@ def call(String openshiftCluster, String openshiftProject, String imageName) {
 //	}
 //}
 
+
 	// Update deployment.yaml with new Docker Hub image
 	sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 	
-	 openshift.withCluster(clustername: ${openshiftCluster}, serverUrlVariable: 'serverURL', tokenVariable: 'token') {
-        	openshift.withProject(openshiftProject) {
-            	sh "oc login --server=${serverURL} --token=${token} --insecure-skip-tls-verify"
-            	sh "oc project ${openshiftProject}"
-            	sh "oc apply -f ."
-        	}
-    	}
+	openshift.withCluster(clustername: openshiftCluster, serverUrlVariable: 'serverURL', tokenVariable: 'token') {
+		openshift.withProject(openshiftProject) {
+			sh "oc login --server=${serverURL} --token=${token} --insecure-skip-tls-verify"
+			sh "oc project ${openshiftProject}"
+			sh "oc apply -f ."
+		}
+	}
 }
+
