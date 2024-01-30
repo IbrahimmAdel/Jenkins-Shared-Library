11dcb39c6a228d8bc79d46011fda812c34ef7583
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-30 23:26:53 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,56 +1,11 @@
 #!/usr/bin/env groovy
-def call(String imageName ,String deploymentFileName ,String openshiftProject ,String openshiftCluster) {
+def call(String openshiftCluster, String openshiftProject, String imageName) {
 	openshift.withCluster("${openshiftCluster}") {
                 openshiftLogin(project: "${openshiftProject}")
 			// Update deployment.yaml with new Docker Hub image
-			sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
+			sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 			
 			// Deploy on OpenShif cluster
 			sh "oc apply -f ." 
 		}
 	}
-
-			
-//def call(String imageName, String deploymentFileName, String openshifProject, String openshiftCluster){ //String imageName, String deploymentFileName, String openshiftCredentialsID, String openshiftServerURL, String openshifProject) {
-  //      sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
-                    
-        // Log in to OpenShift with serviceaccount
-//	sh "oc apply -f . -n ${openshiftProject} --server=${openshiftCluster}"
-
-
-	
-	//withCredentials([string(credentialsId: "${openshiftCredentialsID}", variable: 'OPENSHIFT_TOKEN')]) {
-          //      sh "oc login --token=${OPENSHIFT_TOKEN} --server=${openshiftServerURL} --insecure-skip-tls-verify"
-        //	echo "Deploying the APP to OpenShift..."
-        //	sh "oc apply -f . -n ${openshifProject}"
-        //}      
-//}
-
-//#!usr/bin/env groovy
-//def call(String imageName, String deploymentFileName, String openshiftProject, String openshiftCluster) {
- //   // Update deployment.yaml with new Docker Hub image
-  //  sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
-                    
-   // sh "oc apply -f . -n ${openshiftProject} --server=${openshiftCluster}"
-//}
-//def call(String imageName ,String deploymentFileName ,String openshiftProject ,String openshiftCluster) {
-	// Update deployment.yaml with new Docker Hub image
-	//sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
-
-	 //withCredentials([openshiftServiceAccount(credentialsId: 'OpenShift-sa-token', variable: 'OPENSHIFT_CREDENTIALS')]) {
-                       // sh "oc login --token=${OPENSHIFT_CREDENTIALS} --server=${openshiftCluster}"
-                       // sh "oc apply -f ."
-	
-	// Log in to OpenShift with serviceaccount and deploy manifists files
-	//openshift.withCluster(openshiftCluster) {
-	//	withCredentials([openshiftServiceAccount(credentialsId: 'OpenShift-sa-token', variable: 'OPENSHIFT_CREDENTIALS')]) {
-  		//openshift.withProject(openshiftProject) {
-	//		sh "oc login --token=${OPENSHIFT_CREDENTIALS}"
-	//		sh "oc apply -f ." //-n ${openshiftProject} --server=${openshiftCluster}"	
-		//}
-	//}
-
-
-
-
-
