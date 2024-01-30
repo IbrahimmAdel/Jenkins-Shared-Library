fa3a8f803543812afc790137644706c598f6c71f
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-30 22:54:55 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,4 +1,16 @@
 #!/usr/bin/env groovy
+def call(String imageName ,String deploymentFileName ,String openshiftProject ,String openshiftCluster) {
+	openshift.withCluster(server: openshiftCluster) {
+                openshift.withProject(openshiftProject) {
+			// Update deployment.yaml with new Docker Hub image
+			sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
+			
+			// Deploy on OpenShif cluster
+			sh "oc apply -f ." 
+		}
+	}
+}
+			
 //def call(String imageName, String deploymentFileName, String openshifProject, String openshiftCluster){ //String imageName, String deploymentFileName, String openshiftCredentialsID, String openshiftServerURL, String openshifProject) {
   //      sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
                     
@@ -21,13 +33,13 @@
                     
    // sh "oc apply -f . -n ${openshiftProject} --server=${openshiftCluster}"
 //}
-def call(String imageName ,String deploymentFileName ,String openshiftProject ,String openshiftCluster) {
+//def call(String imageName ,String deploymentFileName ,String openshiftProject ,String openshiftCluster) {
 	// Update deployment.yaml with new Docker Hub image
-	sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
+	//sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
 
-	 withCredentials([openshiftServiceAccount(credentialsId: 'OpenShift-sa-token', variable: 'OPENSHIFT_CREDENTIALS')]) {
-                        sh "oc login --token=${OPENSHIFT_CREDENTIALS} --server=${openshiftCluster}"
-                        sh "oc apply -f ."
+	 //withCredentials([openshiftServiceAccount(credentialsId: 'OpenShift-sa-token', variable: 'OPENSHIFT_CREDENTIALS')]) {
+                       // sh "oc login --token=${OPENSHIFT_CREDENTIALS} --server=${openshiftCluster}"
+                       // sh "oc apply -f ."
 	
 	// Log in to OpenShift with serviceaccount and deploy manifists files
 	//openshift.withCluster(openshiftCluster) {
