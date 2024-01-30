9e725c82a271da63a7308eeb88f74170cc1b5997
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-31 00:04:51 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -5,8 +5,8 @@ def call(String openshiftCluster, String openshiftProject, String imageName) {
 	sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
 	// Login and Deploy on OpenShif cluster
-	openshift.withCluster('${openshiftCluster}') {
-	sh "oc project '${openshiftProject}'"
+	openshift.withCluster(${openshiftCluster}) {
+	sh "oc project ${openshiftProject}"
 	sh "oc apply -f ." 
 	}
 }
