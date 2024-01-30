d9d58d922e14638ca63298481e0746b668457762
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-30 15:39:21 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,4 +1,4 @@
-//#!usr/bin/env groovy
+#!usr/bin/env groovy
 //def call(String imageName, String deploymentFileName, String openshifProject, String openshiftCluster){ //String imageName, String deploymentFileName, String openshiftCredentialsID, String openshiftServerURL, String openshifProject) {
 	// Update deployment.yaml with new Docker Hub image
   //      sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
@@ -15,12 +15,13 @@
         //}      
 //}
 
-#!/usr/bin/env groovy
-def call(String imageName, String deploymentFileName, String openshiftProject, String openshiftCluster) {
-    // Update deployment.yaml with new Docker Hub image
-    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
+//#!usr/bin/env groovy
+//def call(String imageName, String deploymentFileName, String openshiftProject, String openshiftCluster) {
+ //   // Update deployment.yaml with new Docker Hub image
+  //  sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' ${deploymentFileName}"
                     
     // Log in to OpenShift with serviceaccount
-    sh "oc apply -f . -n ${openshiftProject} --server=${openshiftCluster}"
-}
+   // sh "oc apply -f . -n ${openshiftProject} --server=${openshiftCluster}"
+//}
 
+def
