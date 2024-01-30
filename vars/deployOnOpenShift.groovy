76d2a659d12c1bdd6fb107d67417ca30de3db35f
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-31 01:48:13 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,14 +1,30 @@
+//#!/usr/bin/env groovy
+//def call(String openshiftCluster, String openshiftProject, String imageName) {
+    // Update deployment.yaml with new Docker Hub image
+   // sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
+
+  //  openshift.withCluster("${openshiftCluster}") {
+ //       openshift.withProject("${openshiftProject}") {
+ //               //sh "oc login --server="${serverURL}" --token="${token}" --insecure-skip-tls-verify"
+ //               sh "oc project ${openshiftProject}"
+//                sh "oc apply -f ."
+            
+    //    }
+  //  }
+//}
 #!/usr/bin/env groovy
 def call(String openshiftCluster, String openshiftProject, String imageName) {
     // Update deployment.yaml with new Docker Hub image
     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
-    openshift.withCluster("${openshiftCluster}") {
+    openshift.withCluster(clustername: "${openshiftCluster}", serverUrlVariable: 'serverURL', tokenVariable: 'token') {
         openshift.withProject("${openshiftProject}") {
-                //sh "oc login --server="${serverURL}" --token="${token}" --insecure-skip-tls-verify"
+            script {
+                // Use the 'script' block to run multiple 'sh' steps
+                sh "oc login --server=${openshift.getApiServer()} --token=${openshift.getToken()}"
                 sh "oc project ${openshiftProject}"
                 sh "oc apply -f ."
-            
+            }
         }
     }
 }
