1666dcdc43f2a6ee403915bb041aba3ca3203152
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-30 23:35:23 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,11 +1,13 @@
 #!/usr/bin/env groovy
 def call(String openshiftCluster, String openshiftProject, String imageName) {
+	
+	// Update deployment.yaml with new Docker Hub image
+	sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
+
+	// Login and Deploy on OpenShif cluster
 	openshift.withCluster("${openshiftCluster}") {
-                openshiftLogin(project: "${openshiftProject}")
-			// Update deployment.yaml with new Docker Hub image
-			sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
-			
-			// Deploy on OpenShif cluster
-			sh "oc apply -f ." 
+                //openshiftLogin(project: "${openshiftProject}")
+			// 
+			sh "oc apply -f . -n "${openshiftProject}"" 
 		}
 	}
