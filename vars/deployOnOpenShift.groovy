b5f8d27e6e09d71bb2acafe7d2f32fbc506fdec2
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-31 01:29:48 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-//def call(String openshiftCluster, String openshiftProject, String imageName) {
+def call(String openshiftCluster, String openshiftProject, String imageName) {
 	
 	// Update deployment.yaml with new Docker Hub image
 //	sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
@@ -14,28 +14,14 @@
 //	}
 //}
 
-
-def call(String openshiftProject, String imageName) {
-    // Update deployment.yaml with new Docker Hub image
-    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
-
-    // Retrieve OpenShift credentials from Jenkins credentials store
-    withCredentials([openshiftServiceAccount(credentialsId: 'OpenShift', variable: 'openshiftCredentials')]) {
-        // Login and Deploy on OpenShift cluster
-        openshift.withCluster(
-            cluster: 'OpenShift',
-            serverUrlVariable: 'serverURL', 
-            credentialsId: 'OpenShift-token'
-        ) {
-            openshift.withProject("${openshiftProject}") {
-                script {
-                    // Use the 'serverURL' variable here
-                    sh "oc login --token=${openshiftCredentials} --insecure-skip-tls-verify"
-                    sh "oc project ${openshiftProject}"
-                    sh "oc apply -f ."
-                }
-            }
-        }
-    }
+	// Update deployment.yaml with new Docker Hub image
+	sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
+	
+	 openshift.withCluster(clustername: ${openshiftCluster}, serverUrlVariable: 'serverURL', tokenVariable: 'token') {
+        	openshift.withProject(openshiftProject) {
+            	sh "oc login --server=${serverURL} --token=${token} --insecure-skip-tls-verify"
+            	sh "oc project ${openshiftProject}"
+            	sh "oc apply -f ."
+        	}
+    	}
 }
-
