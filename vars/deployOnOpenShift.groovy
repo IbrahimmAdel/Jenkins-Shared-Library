666c9c36ef34aec3662b9b6d994aae2710ff8e77
Ibrahim Adel <ibrahimadel1010@gmail.com>
2024-01-31 10:01:46 +0200

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -1,30 +1,22 @@
 #!/usr/bin/env groovy
-//def call(String openshiftCluster, String openshiftProject, String imageName) {
-    // Update deployment.yaml with new Docker Hub image
-   // sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
-  //  openshift.withCluster("${openshiftCluster}") {
- //       openshift.withProject("${openshiftProject}") {
- //               //sh "oc login --server="${serverURL}" --token="${token}" --insecure-skip-tls-verify"
- //               sh "oc project ${openshiftProject}"
-//                sh "oc apply -f ."
-            
-    //    }
-  //  }
-//}
-
-def call(String OpenShiftCredentialsID, String openshiftCluster, String openshiftProject, String imageName) {
+//OpenShiftCredentialsID can be credentials of service account token or KubeConfig file 
+def call(String OpenShiftCredentialsID, String openshiftClusterurl, String openshiftProject, String imageName) {
+    
     // Update deployment.yaml with new Docker Hub image
     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
+    // login via cluster url & service account token
     withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
-    //openshift.withCluster(clustername: "${openshiftCluster}", serverUrlVariable: 'serverURL', tokenVariable: 'token') {
-        //openshift.withProject("${openshiftProject}") {   
-                // Use the 'script' block to run multiple 'sh' steps
-                sh "oc login --server=${openshiftCluster} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
-                sh "oc project ${openshiftProject}"
-                sh "oc apply -f ."
-        //}
+            sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
+            sh "oc project ${openshiftProject}"
+            sh "oc apply -f ."
     }
+
+    // login via cluster KubeConfig file
+    //withCredentials([file(credentialsId: 'OpenShiftCredentialsID', variable: 'KUBECONFIG_FILE')]) {
+            //sh "export KUBECONFIG=\$KUBECONFIG_FILE && oc apply -f . -n "${openshiftProject}""
+    //}
+    
 }
 
